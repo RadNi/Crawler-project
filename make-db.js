075bccbe53a9bf8b937dc74da02aee8379d7d678
RadNi <amirhossein_kh@ymail.com>
2019-02-04 19:22:48 +0330

    db script added

---
+++ make-db.js
@@ -0,0 +1,28 @@
+// require('events').EventEmitter.defaultMaxListeners = 11
+
+
+const Crawler = require("crawler");
+const DB = require('./custom-db');
+const parser = require('url-parse');
+const lodash = require('lodash');
+
+var db = new DB();
+
+
+
+db.db.run('CREATE TABLE IF NOT EXISTS test (URL TEXT PRIMARY KEY , Host TEXT);', (err) => {
+    if (err) {
+        console.log('ERROR!', err)
+    }
+    else {
+        console.log("Table created successfully")
+    }
+})
+
+
+
+
+// db.parallelize(db.select, "google.com")
+// db.parallelize(db.insert, ["http://pandarian.radni.ir", "radni.ir"])
+// db.close()
+
